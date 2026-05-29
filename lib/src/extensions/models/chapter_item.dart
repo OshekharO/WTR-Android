6fe4825aaafe39f0ecb3c2dea2d04d9eb25193d2
OshekharO <omeeshekher@gmail.com>
2026-05-29 16:21:01 +0530

    Fix chapter item JSON parsing to include 'order' field for number and title

--- lib/src/extensions/models/chapter_item.dart
+++ lib/src/extensions/models/chapter_item.dart
@@ -15,10 +15,10 @@ class ChapterItem {
   factory ChapterItem.fromJson(Map<String, dynamic> json) => ChapterItem(
         id: int.tryParse('${json['id'] ?? json['chapter_id'] ?? 0}') ?? 0,
         number: int.tryParse(
-                '${json['number'] ?? json['chapter_no'] ?? json['no'] ?? 1}') ??
+                '${json['number'] ?? json['chapter_no'] ?? json['no'] ?? json['order'] ?? 1}') ??
             1,
         title:
-            '${json['title'] ?? 'Chapter ${json['chapter_no'] ?? json['number'] ?? ''}'}',
+            '${json['title'] ?? 'Chapter ${json['chapter_no'] ?? json['number'] ?? json['order'] ?? ''}'}',
         updatedAt: DateTime.tryParse('${json['updated_at'] ?? ''}'),
       );
 }
